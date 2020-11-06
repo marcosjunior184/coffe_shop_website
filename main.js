6caab2026753f197349359f35bf779621efa24c8
mad184 <mad184@usask.ca>
2020-11-06 13:55:36 -0600

    upload assignment 3 to the repo

--- main.js
+++ main.js
@@ -3,14 +3,26 @@
 // load package
 const express = require('express');
 const bodyParser = require("body-Parser");
-const fs = require('fs');
 const { get, STATUS_CODES } = require('http');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 const app = express();
- 
-const PATH = __dirname + "/html/";
+const axios = require('axios');
+var mysql  = require('mysql');
+const { json } = require('express');
+
+var con = mysql.createConnection({
+  host : 'localhost',
+  user: 'root',
+  database: 'mydb',
+  password: 'Mad184'
+});
+
+con.connect(function(err){
+  if (err) throw err;
+  console.log("Connected");
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -18,38 +30,41 @@ app.get('/', (req, res) =>{
   res.sendFile(__dirname + "/html/root.html");
 });
 
-app.get('/postmessage/', (req, res) => {
+
+app.get('/postmessage', (req, res) => {
   res.sendFile(__dirname + "/html/posting.html");
 });
 
 
-
-app.set('view engine', 'html');
-
-app.post('/postmessage', (req,res) => {
-
-  console.log(req.body);
-
+app.post('/insert', (req,res) => {
   var topic = req.body.topic_input;
   var data = req.body.data;
   let time= new Date();
 
-  var Hours = "Time - " + time.getHours() + ":" + time.getMinutes() ;
-  var Day = "Date - " + time.getDate()+"/"+time.getMonth();
+  var timestamp = time.getDate()+"/"+time.getMonth()+ "/" + " - "+ time.getHours() + ":" + time.getMinutes() ;
 
-  var aPOst = Day + " -" + Hours + "\nTopic - " + topic + "\n" + data;
+  var sql = "INSERT INTO post (Topic, Data, Timestamp) Values (" + JSON.stringify(topic) + ","+JSON.stringify(data)+","+JSON.stringify(timestamp)+");";
 
-  
-  fs.writeFile('html/' + "post.txt", aPOst + "\n",{ flag: 'a+' }, err => {
-    if (err){
-      console.error(err)
-      return
-    }
-    fs.close;
-    return ;
+  con.query(sql, function(err, result){
+    if (err) throw err;
+    console.log("Inserted");
   });
+
   res.send();
 });
+
+app.post('/select', (req, res) => {
+
+  var fieldType = req.body.type_of_field;
+  var orderType = req.body.type_of_order;
+
+  var sql = "SELECT * FROM post ORDER BY "+fieldType + " " + orderType + ";";
+  con.query(sql, function(err, result, filds){
+    if (err) throw err;
+    res.send(result);
+    });
+});
+
 
 app.use('/', express.static('html'));
 
