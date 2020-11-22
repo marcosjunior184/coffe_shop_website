51239b50c2bda857ef1e526c5558db80cb4fcd27
mad184 <mad184@usask.ca>
2020-11-22 01:12:08 -0600

    add routers to get menu and orderFrom - functions to set orders on the database and linking the order to a customer

--- coffee_server.js
+++ coffee_server.js
@@ -12,7 +12,7 @@ const {json} = require('express');
 var con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    database: 'coffeeshop',
+    database: 'simplecoffee',
     password: 'Mad184'
 })
 
@@ -34,8 +34,59 @@ app.get('/clientLogin', (req, res) => {res.sendFile(__dirname + "/views/clientLo
 
 app.get('/clientLogon', (req, res) => {res.sendFile(__dirname + "/views/clientLogOn.html")});
 
-app.get('/ClientPage', (req, res) => {res.sendFile(__dirname + "/views/")});
+app.get('/clientPage', (req, res) => {res.sendFile(__dirname + "/views/clientPage.html")});
 
+app.get('/staffPage', (req, res) => {res.sendFile(__dirname + "/views/")});
+
+app.get('/menu', (req, res) => {res.sendFile(__dirname + "/views/menu.html")});
+
+app.get('/orderForm', (req, res) => {res.sendFile(__dirname + "/views/orderForm.html")});
+
+//--------------------XHL------------------------------------------//
+
+app.get('/getMenu', getMenu);
+
+app.post('/addToOrder', addToOrder);
+
+
+
+//----------------Functions----------------------------------------//
+
+async function getMenu(req, res){
+    var sql = 'SELECT * FROM menu;';
+
+    con.query(sql, function(err, result, fields){
+        if (err){
+            throw err;
+        }
+        res.send(result);
+    })
+}
+
+async function addToOrder(req, res){
+    var item = req.body.Item;
+    var price = req.body.Price;
+    var orderNumber = req.body.OrderNumber;
+
+
+    var sql = 'INSERT INTO Orders (Item, Quantity, Order_total, OrderNumber) VALUES ('+JSON.stringify(item)+','+ 1 +','+price+','+orderNumber+');';
+    con.query(sql, function(err, result, fields){
+        if (err){
+            throw err;
+        }
+        linkOrder(orderNumber)
+        res.send();
+    })
+}
+
+async function linkOrder(orderNumber){
+    var sql = 'INSERT INTO customers (Order_Number) VALUES ('+orderNumber+');';
+    con.query(sql, function(err, result, fields){
+        if (err){
+            throw err;
+        }
+    })
+}
 
 app.listen(PORT, HOST);
 console.log("up and running");
