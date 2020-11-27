f755eeac98e5e5ef892eeec3c9b12fa792829214
mad184 <mad184@usask.ca>
2020-11-27 00:15:31 -0600

    add new xhl, functions and routers for staff page and orderReview (in staff side)

--- coffee_server.js
+++ coffee_server.js
@@ -27,9 +27,16 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use('/', express.static('views'));
 
+app.use('/', express.static('CSS'));
+
+
 app.get('/', (req, res) => {res.sendFile(__dirname + "/views/indesx.html")});
 
 app.get('/stafflLogin', (req, res) => {res.sendFile(__dirname+ "/views/staffLogin.html")});
+
+app.get('/staffPage', (req, res) => {res.sendFile(__dirname + "/views/staffSide/staffPage.html")});
+
+app.get('/OrderReview', (req, res) => {res.sendFile(__dirname+ "/views/staffSide/OrderReview.html")});
 
 app.get('/clientLogin', (req, res) => {res.sendFile(__dirname + "/views/clientLogin.html")});
 
@@ -37,13 +44,9 @@ app.get('/clientLogon', (req, res) => {res.sendFile(__dirname + "/views/clientLo
 
 app.get('/clientPage', (req, res) => {res.sendFile(__dirname + "/views/clientPage.html")});
 
-app.get('/staffPage', (req, res) => {res.sendFile(__dirname + "/views/")});
-
 app.get('/menu', (req, res) => {res.sendFile(__dirname + "/views/menu.html")});
 
 app.get('/orderForm', (req, res) => {res.sendFile(__dirname + "/views/orderForm.html")});
-
-app.get('/staffPage', (req, res) => {res.sendFile(__dirname + "/views/staffPage.html")});
 
 app.get('/menuEdit', (req, res) => {res.sendFile(__dirname + "/views/menuEdit.html")});
 
@@ -55,9 +58,11 @@ app.get('/myOrder', (req, res) => {res.sendFile(__dirname + "/views/myOrder.html
 
 app.get('/getMenu', getMenu);
 
+app.get('/getOrderReview', getOrders);
+
 app.post('/addToOrder', addToOrder);
 
-
+app.post('/completeOrder', completeOrder);
 
 //----------------Functions----------------------------------------//
 
@@ -94,6 +99,30 @@ async function linkOrder(orderNumber){
         if (err){
             throw err;
         }
+    })
+}
+
+async function getOrders(req, res){
+    var sql = 'SELECT * FROM orders';
+    con.query(sql, function(err, result, fields){
+        if (err){
+            throw  err;
+        }
+        res.send(result);
+    })
+}
+
+async function completeOrder(req, res){
+    const Item = req.body.Item;
+    const OrderNum = req.body.OrderNumber;
+
+    const sql = 'DELETE FROM orders WHERE Item='+JSON.stringify(Item)+' AND OrderNumber='+OrderNum;
+
+    con.query(sql, function(err, result, field){
+        if (err){
+            throw err;
+        }
+        res.send();
     })
 }
 
